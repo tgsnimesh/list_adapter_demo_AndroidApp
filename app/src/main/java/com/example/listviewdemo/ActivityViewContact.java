4adbb79072cc39a70a1ae03c655ed2172ec6b104
tgsnimesh <tgsnimesh@gmail.com>
2024-07-14 02:02:43 +0530

    Implement user contact number dial feature.    - Use Implicit Intent method.

--- app/src/main/java/com/example/listviewdemo/ActivityViewContact.java
+++ app/src/main/java/com/example/listviewdemo/ActivityViewContact.java
@@ -3,7 +3,9 @@ package com.example.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -34,6 +36,15 @@ public class ActivityViewContact extends AppCompatActivity {
         imgView.setImageResource(img);
         txtName.setText(name);
         txtDescription.setText(description);
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+description));
+                startActivity(intent);
+            }
+        });
 
     }
 }
