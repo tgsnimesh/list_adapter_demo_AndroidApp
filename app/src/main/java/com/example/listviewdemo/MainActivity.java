84ed6ea74dafb3f5cbf8b561215f1113945a55b8
tgsnimesh <tgsnimesh@gmail.com>
2024-07-14 00:36:33 +0530

    Create ComplexList view using CustomArrayAdapter    - Create new simple_single_list_view.xml file.    - Defined new array adapter data sources such as.       * name.       * description.       * profilerPict.    - Create CustomArrayAdapter class       * implement constructor.       * override getView() method    - Use LayoutInflater to convert simple list view item into java view object.       * used getSystem service method.

--- app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.listviewdemo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,25 +26,45 @@ public class MainActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.listView);
 
-        String[] countries = new String[]{"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe", "Palestine"};
+        String[] name = {"Sachintha Nimesh", "Dumindu Lakshan", "Vimukthi Lakshan", "Nimesh Dev", "Shay Cormac", "Dumindu Lakshan Wikramasinghe", "Sachintha Nimesh", "Dumindu Lakshan", "Vimukthi Lakshan", "Nimesh Dev", "Shay Cormac", "Dumindu Lakshan Wikramasinghe"};
+        String[] description = {"077 851 9573", "071 055 8339", "072 234 2324", "077 851 9573", "071 055 8339", "072 234 2324", "077 851 9573", "071 055 8339", "072 234 2324", "077 851 9573", "071 055 8339", "072 234 2324"};
+        int[] profilerPict = {R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f, R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f};
 
-        ListAdapter listAdapter =  new ArrayAdapter<String>(getApplicationContext(), R.layout.simple_single_list_view, R.id.txtView, countries);
-        listView.setAdapter(listAdapter);
+        CustomAdapter customAdapter = new CustomAdapter(this, name, description, profilerPict);
+        listView.setAdapter(customAdapter);
+    }
+}
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
+class CustomAdapter extends ArrayAdapter<String> {
 
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, countries[position], Toast.LENGTH_SHORT).show();
-            }
-        });
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @SuppressLint("ResourceAsColor")
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, countries[position], Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        });
+    Context context;
+    String[] name, description;
+    int[] profilePict;
+
+    CustomAdapter(Context context, String[] name, String[] description, int[] profilePict){
+        super(context, R.layout.simple_single_list_view, R.id.txtName, name);
+        this.context = context;
+        this.name = name;
+        this.description = description;
+        this.profilePict = profilePict;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        @SuppressLint("ViewHolder")
+        View view = inflater.inflate(R.layout.simple_single_list_view, parent, false);
+
+        ImageView ivProfilePict = view.findViewById(R.id.imgView);
+        TextView txtName = view.findViewById(R.id.txtName),
+                txtDescription = view.findViewById(R.id.txtDescription);
+
+        ivProfilePict.setImageResource(profilePict[position]);
+        txtName.setText(name[position]);
+        txtDescription.setText(description[position]);
+
+        return view;
     }
 }
